2742cbfeb04b89c09c36731a0a22f6360592fb52
H Waldo G <gwaldo@gmail.com>
2014-06-04 20:37:48 -0400

    adding a go starter, doing some cleanup

---
+++ statme.go
@@ -0,0 +1,6 @@
+// Send metrics
+
+// Intended Usage:
+// statme [-s|--statsd|-g|--graphite] ([-tcp|-udp]) ([-p port]) (-h host) metric value type
+
+// It would be helpful to be able to have some defaults or specify a config file
